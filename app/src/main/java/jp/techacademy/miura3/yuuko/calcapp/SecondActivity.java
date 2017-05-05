6df1951d5c11ea9fb4c5780799a1120386c7051a
yukomiura87 <yukomiura2960@gmail.com>
2017-05-06 00:56:42 +0900

    不要なimportを削除

--- app/src/main/java/jp/techacademy/miura3/yuuko/calcapp/SecondActivity.java
+++ app/src/main/java/jp/techacademy/miura3/yuuko/calcapp/SecondActivity.java
@@ -5,8 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
